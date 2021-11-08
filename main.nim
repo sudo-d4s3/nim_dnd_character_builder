9627a0a6f8cc3f0a018b963bf1291f633f36dde3
d4s3 <honeycutt.chandler@pm.me>
2021-11-07 22:41:13 -0600

    Turned elif soup into iterators
    I changed the long arrays into json files and turned the elif statements
    into for loops

--- main.nim
+++ main.nim
@@ -1,72 +1,65 @@
 import std/strutils
+import std/osproc
+import std/json
+
+let race = ["Dwarf","Elf","Halfling","Human","Dragonborn","Gnome","Half_Elf","Half_Orc","Tiefling"]
+let class = ["Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"]
+
+iterator count_to(n: int): int =
+        var i = 0
+        while i <= n:
+                yield i
+                inc i
 
 proc get_name() =
-        echo "Please Enter Your Character's Name:"
-        let name = readLine(stdin)
+        echo "Please Enter Your Character'S Name:"
+        let usr_name = readline(stdin)
 
 proc get_race_and_subrace() =
-        echo "Please Choose Your Race: (You can type the number or word)"
-        echo "0 - Dwarf\n1 - Elf\n2 - Halfling\n3 - Human\n4 - Dragonborn\n5 - Gnome\n6 - Half-Elf\n7 - Half-Orc\n8 - Tiefling"
-        let race = readLine(stdin)
-        if normalize(race) == "dwarf" or normalize(race) == "0":
-                echo "Please Choose Your Sub-Race:"
-                echo "0 - Hill Dwarf"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "elf" or normalize(race) == "1":
-                echo "Please Choose Your Sub-Race:"
-                echo "0 - High Elf"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "halfling" or normalize(race) == "2":
-                echo "Please Choose Your Sub-Race:"
-                echo "0 - Lightfoot"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "human" or normalize(race) == "3":
-                echo "Please Choose Your Sub-Race:"
-                echo "Sorry the SRD doesn't have any subraces for Humans"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "dragonborn" or normalize(race) == "4":
-                echo "Please Choose Your Sub-Race:"
-                echo "0 - Black\n1 - Blue\n2 - Brass\n3 - Bronze\n4 - Copper\n5 - Gold\n6 - Green\n7 - Red\n8 - Silver\n9 - White"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "Gnome" or normalize(race) == "5":
-                echo "Please Choose Your Sub-Race:"
-                echo "0 - Rock Gnome"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "Half-Elf" or normalize(race) == "6":
-                echo "Please Choose Your Sub-Race:"
-                echo "Sorry the SRD doesn't have any subraces for Half-Elves"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "Half-Orc" or normalize(race) == "7":
-                echo "Please Choose Your Sub-Race:"
-                echo "Sorry the SRD doesn't have any subraces for Half-Orcs"
-                let subrace = readLine(stdin)
-        elif normalize(race) == "Tiefling" or normalize(race) == "8":
-                echo "Please Choose Your Sub-Race:"
-                echo "Sorry the SRD doesn't have any subraces for Tieflings"
-                let subrace = readLine(stdin)
-        else:
-                get_race_and_subrace()
+        echo "Please Choose Your Race: (Numbers Don't Work Yet)"
+        let files = execCmdEx("ls srd/race/")
+        var num = 0
+        try:
+                for race in split(files[0]):
+                        let json_object = parseFile("srd/race/" & race )
+                        echo num, " - ", json_object{"race"}.getStr
+                        num += 1
+        finally:
+                let usr_race = normalize(readline(stdin))
+                let json_object = parseFile("srd/race/" & usr_race & ".json")
+                echo "Please Choose Your Subrace:"
+                num = 0
+                for subrace in json_object{"subrace"}:
+                        echo num, " - ", subrace.getStr
+                        num += 1
+                let usr_subrace = normalize(readline(stdin))
+                return
         
 proc get_class() =
-        echo "0 - Barbarian\n1 - Bard\n2 - Cleric\n3 - Druid\n4 - Fighter\n5 - Monk\n6 - Paladin\n7 - Ranger\n8 - Rogue\n9 - Sorcerer\n10 - Warlock\n11 - Wizard\n(Only numbers work)"
-        let class = readLine(stdin)
-        if normalize(class) == "0" or normalize(class) == "1" or normalize(class) == "2" or normalize(class) == "3" or normalize(class) == "4" or normalize(class) == "5" or normalize(class) == "6" or normalize(class) == "7" or normalize(class) == "8" or normalize(class) == "9" or normalize(class) == "10" or normalize(class) == "11":
-                echo ""
-        else:
-                get_class()
+        echo "Please Choose Your Class:"
+        let files = execCmdEX("ls srd/class/")
+        var num = 0
+        try:
+                for class in split(files[0]):
+                        let json_object = parseFile("srd/class/" & class )
+                        echo num, " - ", json_object{"class"}.getStr
+                        num += 1
+        finally:
+                let usr_class = normalize(readline(stdin))
+                return
 
 proc get_ability_scores() =
-        echo "Please Set Your Ability Scores (Only Standard Array Works rn)"
-        echo "Strength:"
-        let str = readLine(stdin)
-        echo "Dexterity:"
-        let dex = readLine(stdin)
-        echo "Intelligence:"
-        let int = readLine(stdin)
-        echo "Wisdom:"
-        let wis = readLine(stdin)
-        echo "Charisma:"
-        let cha = readLine(stdin)
+        echo "Please Set Your Ability Scores (Only Standard Array Works Rn)"
+        echo "strength:"
+        let str = readline(stdin)
+        echo "dexterity:"
+        let dex = readline(stdin)
+        echo "intelligence:"
+        let int = readline(stdin)
+        echo "wisdom:"
+        let wis = readline(stdin)
+        echo "charisma:"
+        let cha = readline(stdin)
 
 get_name()
 get_race_and_subrace()
